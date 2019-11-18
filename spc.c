648f7507275868493c336631a12325be88f2a391
tikaradate <tikaradate@gmail.com>
2019-11-18 01:04:48 -0300

    varias coisas feitas, a fazer: barreira; ataque dos aliens e velocidades

--- spc.c
+++ spc.c
@@ -81,11 +81,13 @@ void desenha_aliens(t_lista *aliens){
 	int i, j;
 	
 	inicializa_atual_inicio(aliens);
-	for(i = 0; i < ALI_COL; i++){
+	for(i = 0; i < aliens->tamanho; i++){
 		inicializa_atual_inicio(aliens->atual->u.col);	
-		for(j = 0; j < ALI_LIN; j++){
-			desenha_item(aliens->atual->u.col->atual);
-			incrementa_atual(aliens->atual->u.col);
+		for(j = 0; j < aliens->atual->u.col->tamanho; j++){
+			if(aliens->atual->u.col->atual->u.estado == VIVO){
+				desenha_item(aliens->atual->u.col->atual);
+				incrementa_atual(aliens->atual->u.col);
+			}
 		}
 		incrementa_atual(aliens);
 	}
@@ -97,14 +99,14 @@ void move_aliens(int *direcao, t_lista *aliens){
 	inicializa_atual_inicio(aliens);
 	vert = 0;
 	if(aliens->ini->prox->u.col->ini->prox->pos.x - 1 < 0 ||
-	   aliens->fim->prev->u.col->ini->prox->pos.x + 1 > MIN_X - 5){
+	   aliens->fim->prev->u.col->ini->prox->pos.x + 1 > MIN_X- 5){
 		*direcao *= -1;  /* inverte sentido do movimento horizontal */
 		vert = BAIXO;
 	}
 
-	for(i = 0; i < ALI_COL; i++){
+	for(i = 0; i < aliens->tamanho; i++){
 		inicializa_atual_inicio(aliens->atual->u.col);	
-			for(j = 0; j < ALI_LIN; j++){
+			for(j = 0; j < aliens->atual->u.col->tamanho; j++){
 			move_item(*direcao, aliens->atual->u.col->atual);
 			move_item(vert, aliens->atual->u.col->atual);
 			incrementa_atual(aliens->atual->u.col);
@@ -112,6 +114,7 @@ void move_aliens(int *direcao, t_lista *aliens){
 		incrementa_atual(aliens);
 	}
 }
+
 void move_item(int sentido, t_nodo *item){
 	switch(sentido){
 	case DIREI:
@@ -130,25 +133,33 @@ void move_item(int sentido, t_nodo *item){
 }
 
 void inicializa_tiro(t_lista *tiro){
-	t_coord pos;
-
-	pos.x = 50;
-	pos.y = 36;
 	inicializa_lista(tiro);
-	insere_fim_lista(VIVO, TIRO, pos, tiro);
-	inicia_sprite(tiro->ini->prox);
 }
 
 void desenha_tiro(t_lista *tiro){
-	desenha_item(tiro->ini->prox);
+	int i;
+	
+	inicializa_atual_inicio(tiro);
+	for(i = 0; i < tiro->tamanho; i++){
+		if(tiro->atual->u.estado == VIVO){
+			desenha_item(tiro->atual);
+			incrementa_atual(tiro);
+		}
+	}
 }
 
 void move_tiro(t_lista *tiro){
-	int tecla;
-
-	tecla = getch();
-	if(tecla == ' '){
-		move_item(CIMA, tiro->ini->prox);
+	if(!lista_vazia(tiro)){
+		int i, aux;
+		
+		inicializa_atual_inicio(tiro);
+		for(i = 0; i < tiro->tamanho; i++){
+			move_item(CIMA, tiro->atual);
+			incrementa_atual(tiro);
+		}
+		inicializa_atual_fim(tiro);
+		if(tiro->atual->pos.y < 0)
+			remove_fim_lista(&aux, tiro);
 	}
 }
 
@@ -166,8 +177,9 @@ void desenha_canhao(t_lista *canhao){
 	desenha_item(canhao->ini->prox);
 }
 
-void move_canhao(t_lista *canhao){
+void move_canhao(t_lista *canhao, t_lista *tiro){
 	int tecla;
+	t_coord pos_tiro;
 
 	tecla = getch();
 	if(tecla == KEY_LEFT){
@@ -176,11 +188,96 @@ void move_canhao(t_lista *canhao){
 	} else if(tecla == KEY_RIGHT){
 		if(canhao->ini->prox->pos.x < 100 - 5)
 			move_item(DIREI, canhao->ini->prox);
+	} else if(tecla == ' '){
+		if(tiro->tamanho < 3){
+			inicializa_atual_inicio(canhao);
+			pos_tiro = canhao->atual->pos;
+			pos_tiro.x += 2;
+			pos_tiro.y += -1;
+			insere_fim_lista(VIVO, TIRO, pos_tiro, tiro);
+			inicializa_atual_fim(tiro);
+			inicia_sprite(tiro->atual);
+		}
 	}
+}
+
+void detecta_colisoes(t_lista *tiro, t_lista *aliens){
+	int i, j, k;
+
+	inicializa_atual_inicio(tiro);
+	for(i = 0; i < tiro->tamanho; i++){
+
+		inicializa_atual_inicio(aliens);
+		for(j = 0; j < aliens->tamanho; j++){
+
+			inicializa_atual_inicio(aliens->atual->u.col);
+			for(k = 0; k < aliens->atual->u.col->tamanho; k++){
+				if(tiro->atual->pos.x >= aliens->atual->u.col->atual->pos.x &&
+				   tiro->atual->pos.x <= aliens->atual->u.col->atual->pos.x + 4 &&
+				   tiro->atual->pos.y >= aliens->atual->u.col->atual->pos.y &&
+				   tiro->atual->pos.y <= aliens->atual->u.col->atual->pos.y + 2){
+					tiro->atual->u.estado = MORTO;
+					aliens->atual->u.col->atual->u.estado = MORTO;
+				}
+				incrementa_atual(aliens->atual->u.col);
+			}
+			incrementa_atual(aliens);
+		}
+		incrementa_atual(tiro);
+	}
+}
+
+void remove_tiro(t_lista *tiro){
+	int i, lixo;
+	t_nodo *aux;
+
+	inicializa_atual_inicio(tiro);
+	for(i = 0; i < tiro->tamanho; i++){
+		if(tiro->atual->u.estado == MORTO){
+			aux = tiro->atual->prox;
+			remove_item_atual(&lixo, tiro);
+			tiro->atual = aux;
+		} else {
+			incrementa_atual(tiro);
+		}
+	}	
+}
+
+void remove_aliens(t_lista *aliens){
+	int i, j, lixo;
+	t_nodo *aux;
+
+	inicializa_atual_inicio(aliens);
+	for(i = 0; i < aliens->tamanho; i++){
+
+		inicializa_atual_inicio(aliens->atual->u.col);
+		for(j = 0; j < aliens->atual->u.col->tamanho; j++){
+			if(aliens->atual->u.col->atual->u.estado == MORTO){
+				aux = aliens->atual->u.col->atual->prox;
+				remove_item_atual(&lixo, aliens->atual->u.col);
+				aliens->atual->u.col->atual = aux;
+			} else {
+				incrementa_atual(aliens->atual->u.col);
+			}
+		}
+
+		incrementa_atual(aliens);
+	}
+	
+	if(aliens->ini->prox->u.col->tamanho == 0)
+		remove_inicio_lista(&lixo, aliens);
+	if(aliens->fim->prev->u.col->tamanho == 0)
+		remove_fim_lista(&lixo, aliens);
+}
+
+
+void remove_morto(t_lista *tiro, t_lista *aliens){
+	remove_tiro(tiro);
+	remove_aliens(aliens);
 }
 int main() {
 	int x_term, y_term, vert;;
-	int i, j, num_col, dir;
+	int i, j, num_col, dir, cont;
 	t_lista aliens, canhao, tiro;
 
 	initscr();              /* inicia a tela */
@@ -197,23 +294,33 @@ int main() {
     keypad(stdscr, TRUE);   /* permite a leitura das setas */
     curs_set(FALSE);        /* não mostra o cursor na tela */
 
-	inicializa_tiro(&tiro);
 	inicializa_aliens(&aliens);
 	inicializa_canhao(&canhao);
+	inicializa_tiro(&tiro);
 	tamanho_lista(&num_col, &aliens);
-	dir = 1;
+	dir = DIREI;
+	cont = 0;
 	while(1){
 		erase();
+		
+		if(cont == 5){
+			move_aliens(&dir, &aliens);
+		}
 
-		move_aliens(&dir, &aliens);
-		desenha_aliens(&aliens);
-		move_canhao(&canhao);
-		desenha_canhao(&canhao);
+		move_canhao(&canhao, &tiro);
 		move_tiro(&tiro);
+		
+		detecta_colisoes(&tiro, &aliens);
+		remove_morto(&tiro, &aliens);
 		desenha_tiro(&tiro);
+		desenha_aliens(&aliens);
+		desenha_canhao(&canhao);
 
 		refresh();
-		usleep(40000);
+		cont++;
+		if(cont > 10)
+			cont = 0;
+		usleep(20000);
 	}
 }
 
